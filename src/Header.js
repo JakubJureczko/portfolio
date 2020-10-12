0f3a3a8143fb7199fca0e8cd0e572a6fb6b01a13
Jakub Jureczko <untech.untech@gmail.com>
2020-10-12 19:16:37 +0200

    header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import jurand from './assets/images/jurandcar.jpeg'
-
+import "./Header.css"
  function Header() {
   
 
   return (
    <div className="nav">
-     
+     <h1>Jakub Jureczko</h1>
+     <p>Web and App developer</p>
    </div>
   )
 }
