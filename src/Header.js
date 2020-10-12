638fbda3456e6c7623e5bbc42c050e4fd9956d09
Jakub Jureczko <untech.untech@gmail.com>
2020-10-12 14:14:16 +0200

    some images and header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import jurand from './assets/images/jurandcar.jpeg'
+
+ function Header() {
+  
+
+  return (
+   <div className="nav">
+     
+   </div>
+  )
+}
+
+export default Header; 
