68b11dbebf170d4e8dda3e2144fd5f5bd24a2be2
Jakub Jureczko <untech.untech@gmail.com>
2020-10-14 15:08:51 +0200

    colors

--- src/Skills.js
+++ src/Skills.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import "./Skills.css"
+import React from "react";
+import "./Skills.css";
 
 function Skills() {
   return (
     <div className="skills">
       <h2>Skills:</h2>
+      <div className="skill-container">
+        <div className="card">
+          <p>
+            <b>Front-End</b>
+          </p>
+          <p>Languages:</p>
+          <span>HTML, CSS, JavaScript</span>
+          <span>React, Next.js</span>
+          <p className="para">Tools:</p>
+          <span>GIMP, Figma, GitHub, Netlify, Vercel</span>
+        </div>
+        <div className="card">
+          <p>
+            <b>Back-End</b>
+          </p>
+          <p>Languages:</p>
+          <span>Node.js, SQL</span>
+          <p className="para2">Tools:</p>
+          <span>Express, MongoDB, Heroku</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
 
 export default Skills;
