9acc82eafefaa14b116586f398a81af64ddf2a13
Jakub Jureczko <untech.untech@gmail.com>
2020-11-06 15:50:38 +0100

    adding skills

--- src/Skills.js
+++ src/Skills.js
@@ -13,15 +13,17 @@ function Skills() {
             <b>Front-End</b>
           </p>
           <span>HTML5, CSS3, JavaScript</span>
-          <span>React, Next.js, Styled Components</span>
+          <span>React, Next.js, TypeScript</span>
+          <span>Styled Components, Sass</span>
         </div>
         <div className="card">
           <span className="card-back"></span>
           <p>
             <b>Back-End</b>
           </p>
-          <span>Node.js, MySQL</span>
-          <span>Express, MongoDB,  Mongoose </span>
+          <span>Node.js, MySQL, Express</span>
+          <span>MongoDB, GraphQL, Mongoose </span>
+          <span>Contentful</span>
         </div>
       </div>
     </div>
